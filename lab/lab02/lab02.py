3abdc248726b4571000afb4a1b25f5e983ab0a3c
AEliu <17284002+AEliu@users.noreply.github.com>
2025-01-12 15:54:58 +0800

    end lab 02 Q4

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -14,6 +14,7 @@ def composite_identity(f, g):
     False
     """
     "*** YOUR CODE HERE ***"
+    return lambda x : f(g(x)) == g(f(x))
 
 
 def sum_digits(y):
@@ -60,6 +61,7 @@ def count_cond(condition):
     8
     """
     "*** YOUR CODE HERE ***"
+    
 
 
 def multiple(a, b):
