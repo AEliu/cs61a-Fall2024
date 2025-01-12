4fe43ad159af9fae393342ce45fce7257ae5b99e
AEliu <17284002+AEliu@users.noreply.github.com>
2025-01-12 11:20:59 +0800

    ND PROBLEM 8 of hog

--- homework/hog/hog.py
+++ homework/hog/hog.py
@@ -234,6 +234,12 @@ def make_averaged(original_function, times_called=1000):
     """
     # BEGIN PROBLEM 8
     "*** YOUR CODE HERE ***"
+    def avg(*agrs):
+        sum = 0
+        for _ in range(times_called):
+            sum += original_function(*agrs)
+        return sum / times_called
+    return avg
     # END PROBLEM 8
 
 
