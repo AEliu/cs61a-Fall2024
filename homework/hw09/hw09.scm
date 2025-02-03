50536a337134def6ad43db1757ecd4f85925243a
AEliu <17284002+AEliu@users.noreply.github.com>
2025-02-03 20:24:11 +0800

    complete curry cook and consum

--- homework/hw09/hw09.scm
+++ homework/hw09/hw09.scm
@@ -1,7 +1,18 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body) 
+  ; (if (= 1 (length formals))
+  ;   `(lambda (,(car formals)) ,body)
+  ;   `(lambda (,(car formals)) (lambda ,(cdr formals) ,body)))
+  (if (null? formals)
+    body
+    `(lambda (,(car formals)) ,(curry-cook (cdr formals) body)))
+)
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+    curry
+    (curry-consume (curry (car args)) (cdr args)))
+  
+)
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
