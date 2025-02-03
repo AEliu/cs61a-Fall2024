114dcaa2272476899d57641dfd3767760139b9a5
AEliu <17284002+AEliu@users.noreply.github.com>
2025-02-03 13:18:34 +0800

    env.lookup(expr) return a Procedure

--- project/scheme/scheme_eval_apply.py
+++ project/scheme/scheme_eval_apply.py
@@ -34,6 +34,8 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        procedure = scheme_eval(first, env)
+        return scheme_apply(procedure, rest.map(lambda r: scheme_eval(r, env)), env)
         # END PROBLEM 3
 
 def scheme_apply(procedure, args, env):
@@ -45,11 +47,17 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
-        
+        args_list = []
+        while args is not nil:
+            args_list.append(args.first)
+            args = args.rest
+        if procedure.need_env:
+            args_list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            return procedure.py_func(*args_list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
